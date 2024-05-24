d4b6d4410a080d01a7b0e159e5b18dd654ad3fb6
RoyalRoader <sksksdlqk@naver.com>
2024-05-24 15:26:57 +0900

    헬스 체크 추가

--- src/main/java/consumer/Controller.java
+++ src/main/java/consumer/Controller.java
@@ -1,5 +1,8 @@
 package consumer;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +26,20 @@ public class Controller {
 	public Mono<ResponseEntity<String>> getHealthCheckKafka() throws Exception {
 		return Mono.just(ResponseEntity.ok("TEST RESPONSE"));
 	}
+	
+	/**
+	 * [EKS] POD LivenessProbe 헬스체크
+	 */
+	private final Instant started = Instant.now();
+	
+    @GetMapping("/healthz")
+    public ResponseEntity<String> healthCheck() {
+        Duration duration = Duration.between(started, Instant.now());
+        if (duration.getSeconds() > 10) {
+            return ResponseEntity.status(500).body("error: " + duration.getSeconds());
+        } else {
+            return ResponseEntity.ok("ok");
+        }
+    }
 
 }
